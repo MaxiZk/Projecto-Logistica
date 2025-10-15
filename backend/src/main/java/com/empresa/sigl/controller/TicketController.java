4e272bdd1439cc1fb42f7a5ea5cdaa852ca4f204
MaxiZk <95772891+MaxiZk@users.noreply.github.com>
2025-10-15 10:17:06 -0300

    Update TicketController.java

--- backend/src/main/java/com/empresa/sigl/controller/TicketController.java
+++ backend/src/main/java/com/empresa/sigl/controller/TicketController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @CrossOrigin(origins = {
         "http://127.0.0.1:5173","http://localhost:5173",
         "http://127.0.0.1:5174","http://localhost:5174",
-        "https://projecto-logistica-l4au.vercel.app"
+        "https://max.zuidnet.online/index.html#home"
 })
 public class TicketController {
     private final TicketService service;
