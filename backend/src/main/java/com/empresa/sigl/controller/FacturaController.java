44c8733c769094418de7e0a6c489b3a709819a9f
MaxiZk <95772891+MaxiZk@users.noreply.github.com>
2025-10-15 10:16:23 -0300

    Update FacturaController.java

--- backend/src/main/java/com/empresa/sigl/controller/FacturaController.java
+++ backend/src/main/java/com/empresa/sigl/controller/FacturaController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/facturas")
-@CrossOrigin(origins = {"http://localhost:5173","http://127.0.0.1:5173"})
+@CrossOrigin(origins = {"http://localhost:5173,http://127.0.0.1:5173,https://max.zuidnet.online/index.html#home"})
 public class FacturaController {
 
     private final FacturaRepository repository;
